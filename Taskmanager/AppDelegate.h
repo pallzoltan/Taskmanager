bb1098f9c5d8156951da8ac10ff005969301ea16
Páll Zoltán <pallzoltan@gmail.com>
2015-02-05 19:07:20 +0100

    Initial Commit

---
+++ Taskmanager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Taskmanager
+//
+//  Created by Páll Zoltán on 05/02/15.
+//  Copyright (c) 2015 Páll Zoltán. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Taskmanager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Taskmanager
+//
+//  Created by Páll Zoltán on 05/02/15.
+//  Copyright (c) 2015 Páll Zoltán. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
